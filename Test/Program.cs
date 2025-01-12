ccbd57068f05705154f38455b4c3bc5976347b2b
sean t <sean@ispyconnect.com>
2025-01-12 09:16:44 +0800

    Update test app to use net9.0

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,10 +10,14 @@ namespace Test
             var plugins = new string[] { "Demo", "Barcode", "Gain", "TensorFlow", "Weather" };
             foreach (var plugin in plugins)
             {
-                var path = System.Reflection.Assembly.GetEntryAssembly().Location;
-                Assembly ass = Assembly.LoadFrom(path.Substring(0, path.IndexOf(@"\Test\")) + @"\"+ plugin+@"\bin\Debug\netstandard2.0\"+plugin+".dll");
-                var ins = ass.CreateInstance("Plugins.Main", true);
-                Console.WriteLine("Instantiated "+plugin+".dll");
+                try
+                {
+                    var path = System.Reflection.Assembly.GetEntryAssembly().Location;
+                    Assembly ass = Assembly.LoadFrom(path.Substring(0, path.IndexOf(@"\Test\")) + @"\" + plugin + @"\bin\Debug\net9.0\" + plugin + ".dll");
+                    var ins = ass.CreateInstance("Plugins.Main", true);
+                    Console.WriteLine("Instantiated " + plugin + ".dll");
+                }
+                catch (Exception ex) { Console.WriteLine(ex.ToString()); }
             }
             Console.ReadLine();
 
